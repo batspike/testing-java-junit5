591b13c0b84213c73f5837d2b0a6ed83509f96a9
Sam Lau <samlau.sam@gmail.com>
2020-08-11 11:09:36 +0800

    @DisplayName

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -3,8 +3,13 @@ package guru.springframework.sfgpetclinic.controllers;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+@TestMethodOrder(OrderAnnotation.class)
 class IndexControllerTest {
 	IndexController controller;
 
@@ -14,11 +19,14 @@ class IndexControllerTest {
 	}
 
 	@Test
+	@DisplayName("Test Proper View name is returned")
+	@Order(1)
 	void testIndex() {
 		assertEquals("index", controller.index());
 	}
 
 	@Test
+	@Order(2)
 	void testOupsHandler() {
 		assertTrue("notimplemented".equals(controller.oupsHandler()), () -> "Expensive Message");
 	}
