9a0eaeae4796c3655c59c9bdc1c2940953011ba2
Sam Lau <samlau.sam@gmail.com>
2020-08-11 09:16:40 +0800

    nested assertAll example

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -0,0 +1,27 @@
+package guru.springframework.sfgpetclinic.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OwnerTest {
+
+	@Test
+	void nestedAssertAll() {
+		Owner owner = new Owner(1L, "Joe", "Buck");
+		owner.setCity("Key West");
+		owner.setTelephone("33993908403");
+		
+		assertAll(	"Properties Test", 
+					() -> assertAll("Person Properties", 
+									() -> assertEquals("Joes", owner.getFirstName(), "First Name error"),
+									() -> assertEquals("Buck", owner.getLastName())
+								   ),
+					() -> assertAll("Owner Properties", 
+									() -> assertEquals("Key Wests", owner.getCity()),
+									() -> assertEquals("33993908403", owner.getTelephone())
+								   )
+				 );
+	}
+
+}
