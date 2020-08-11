909bbd1baa7a6226dd72f71814e269b26da541be
Sam Lau <samlau.sam@gmail.com>
2020-08-11 18:26:27 +0800

    add BeforeAll to test interface

--- src/test/java/guru/springframework/sfgpetclinic/model/ModelTests.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/ModelTests.java
@@ -1,8 +1,16 @@
 package guru.springframework.sfgpetclinic.model;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
 
 @Tag("model")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public interface ModelTests {
 // test interface is useful to define common properties, e.g. @Tag
+	
+	@BeforeAll
+	default void beforeAll() {
+		System.out.println("Do something at the start of class!");
+	}
 }
