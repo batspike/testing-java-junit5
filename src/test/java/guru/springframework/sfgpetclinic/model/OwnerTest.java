37825b571c3b015743e8686ced08073673ab4c42
Sam Lau <samlau.sam@gmail.com>
2020-08-12 09:36:38 +0800

    @ParameterizedTest with @EnumSource

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class OwnerTest implements ModelTests {
@@ -34,4 +35,14 @@ class OwnerTest implements ModelTests {
 	void testValueSource(String val) {
 		System.out.println(val);
 	}
+	
+	@DisplayName("Enum Source Test")
+	@ParameterizedTest(name="{displayName} - [{index}] {arguments}")
+	@EnumSource(OwnerType.class)
+	void testEnumSource(OwnerType ownerType) {
+		System.out.println(ownerType);
+	}
+	
+	
+	
 }
