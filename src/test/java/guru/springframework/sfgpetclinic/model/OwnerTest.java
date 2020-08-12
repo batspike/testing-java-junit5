2f32ada5170897fbdcc46bb1d707755874153519
Sam Lau <samlau.sam@gmail.com>
2020-08-12 10:40:30 +0800

    @ParameterizedTest with @MethodSource

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -2,13 +2,17 @@ package guru.springframework.sfgpetclinic.model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class OwnerTest implements ModelTests {
@@ -63,6 +67,15 @@ class OwnerTest implements ModelTests {
 		System.out.println(stateName + " = "+ val1 + ":"+ val2);
 	}
 	
+	@DisplayName("Method Source Test")
+	@ParameterizedTest(name="{displayName} - [{index}] {arguments}")
+	@MethodSource("getArgs")
+	void testMethodSource(String stateName, int val1, int val2) {
+		System.out.println(stateName + " = "+ val1 + ":"+ val2);
+	}
 	
+	static Stream<Arguments> getArgs() {
+		return Stream.of(Arguments.of("FL", 1, 1), Arguments.of("OH", 2, 2), Arguments.of("MI", 3, 3));
+	}
 	
 }
