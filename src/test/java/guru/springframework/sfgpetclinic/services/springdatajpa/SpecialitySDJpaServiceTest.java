382912e0c5e0c7dea04601c4efec391b742b8c41
Sam Lau <samlau.sam@gmail.com>
2020-08-13 10:32:18 +0800

    verify interaction with mockito

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -1,6 +1,11 @@
 package guru.springframework.sfgpetclinic.services.springdatajpa;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.atMost;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -26,6 +31,15 @@ class SpecialitySDJpaServiceTest {
 	@Test
 	void testDeleteById() {
 		service.deleteById(1L);
+		verify(specialtyRepository).deleteById(1L);//verify the deleteById(1L) was deleted once (default)
+		
+		service.deleteById(1L); // delete one more time
+		verify(specialtyRepository, times(2)).deleteById(1L);//verify the deleteById(1L) was deleted twice
+		verify(specialtyRepository, atLeastOnce()).deleteById(1L);//verify the deleteById(1L) was deleted at least once
+		verify(specialtyRepository, atMost(4)).deleteById(1L);//verify the deleteById(1L) was deleted at most 4 times
+
+		verify(specialtyRepository, never()).deleteById(4L);//verify the deleteById(4L) was never called
+		
 	}
 
 	@Test
